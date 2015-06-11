5b28854c10a38f26779ee265a269db33acec66ff
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-06-11 12:13:58 +0800

    complete superposition_operation/collection_operation/practice_2 && clean code

--- practices/superposition_operation/collection_operation/practice_2.js
+++ practices/superposition_operation/collection_operation/practice_2.js
@@ -2,12 +2,6 @@
 var _ = require('../../my_lodash/my_lodash.js');
 
 function hybrid_operation_to_uneven(collection) {
-    // var get_odd = _.filter_in(collection,function (num) {
-    //     return num % 2 === 1;
-    // });
-    // var result = _.map(get_odd,function (num) {
-    //     return num * 3 + 2;
-    // });
     return _(collection).filter(function (item,i) {
         return item % 2 === 1;
     }).map(function (item) {
