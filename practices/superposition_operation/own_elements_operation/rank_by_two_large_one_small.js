99eb810f8c1b977f09b9f3f3a00411a2ccd945d2
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-06-11 12:44:43 +0800

    complete superposition_operation/own_element_operation/rank_by_two_large_one_small

--- practices/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
+++ practices/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
@@ -3,15 +3,18 @@ var _ = require('../../my_lodash/my_lodash.js');
 
 function rank_by_two_large_one_small(collection){
     var result = [];
-    result = _.bubble_sort(collection);
-    for (var i = 0; i < result.length; i++) {
+    result = _(collection).bubble_sort(function (item_a,item_b) {
+        return item_a > item_b;
+    }).value();
+
+    _(result).each(function (item,i) {
         if (i % 3 === 2) {
-            var num = result[i - 2];
+            var num  = result[i - 2];
             result[i - 2] = result[i - 1];
             result[i - 1] = result[i];
             result[i] = num;
         }
-    }
+    });
     return result;
 }
 module.exports = rank_by_two_large_one_small;
