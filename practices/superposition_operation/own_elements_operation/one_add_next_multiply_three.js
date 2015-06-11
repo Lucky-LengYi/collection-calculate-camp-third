33bfd1185da791e8dc51d534534bfa261f56c5ed
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-06-11 12:39:38 +0800

    complete superposition_operation/own_element_operation/one_add_next_multiply_three

--- practices/superposition_operation/own_elements_operation/one_add_next_multiply_three.js
+++ practices/superposition_operation/own_elements_operation/one_add_next_multiply_three.js
@@ -4,9 +4,9 @@ var _ = require('../../my_lodash/my_lodash.js');
 function one_add_next_multiply_three(collection){
     var result = [];
     var len = collection.length;
-    _.each(collection,function (num,i) {
+    _(collection).each(function (item,i) {
         if (i < len - 1) {
-            result[result.length] = (collection[i] + collection[i+1]) * 3;
+            result.push((item + collection[i+1]) * 3);
         }
     });
     return result;
