22fb4a859e99d607c018f7debc800f74626f760c
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-06-11 10:18:08 +0800

    complete filter/two_collection_practice_2

--- practices/filter/two_collections_practice_2.js
+++ practices/filter/two_collections_practice_2.js
@@ -3,12 +3,11 @@ var _ = require('../my_lodash/my_lodash.js');
 
 function choose_no_common_elements(collection_a, collection_b) {
     var result = [];
-    _.each(collection_a,function (letter) {
-        if (!_.exist(collection_b,letter)) {
-            result[result.length] = letter;
+    _(collection_a).each(function (item,i) {
+        if (!_(collection_b).exist(item).value()) {
+            result.push(item);
         }
     });
-
     return result;
 }
 
