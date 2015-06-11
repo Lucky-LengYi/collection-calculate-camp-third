28c1806d03a8c7866a2daec6bda79f4c159b79f5
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-06-11 10:08:47 +0800

    complete filter/choose_multiples_of_three

--- practices/filter/choose_multiples_of_three.js
+++ practices/filter/choose_multiples_of_three.js
@@ -2,10 +2,9 @@
 var _ = require('../my_lodash/my_lodash.js');
 
 function choose_multiples_of_three(collection) {
-    return _.filter_in(collection,function (num) {
-        return num % 3 === 0;
-    });
-
+    return _(collection).filter(function (item,i) {
+        return item % 3 === 0;
+    }).value();
 }
 
 module.exports = choose_multiples_of_three;
