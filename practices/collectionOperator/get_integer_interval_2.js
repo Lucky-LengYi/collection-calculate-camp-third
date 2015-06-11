29aedf7d2fe37e5d178ec98943dda87e490284ad
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-06-11 09:29:35 +0800

    完成本函数

--- practices/collectionOperator/get_integer_interval_2.js
+++ practices/collectionOperator/get_integer_interval_2.js
@@ -2,11 +2,10 @@
 var _ = require('../my_lodash/my_lodash.js');
 
 function get_integer_interval_2(number_a, number_b) {
-    var result = _.range(number_a,number_b);
-    result = _.filter_in(result,function (num) {
-        return num % 2 === 0;
-    });
-    return result;
+
+    return _().range(number_a,number_b).filter(function (item,i) {
+        return item % 2 === 0;
+    }).value();
 
 }
 
