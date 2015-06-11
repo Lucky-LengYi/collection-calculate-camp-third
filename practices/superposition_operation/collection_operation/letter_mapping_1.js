c5220141d2543d1550127b0ec2395ecc46792301
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-06-11 12:07:54 +0800

    complete superposition_operation/collection_operation/letter_mapping_1

--- practices/superposition_operation/collection_operation/letter_mapping_1.js
+++ practices/superposition_operation/collection_operation/letter_mapping_1.js
@@ -2,14 +2,11 @@
 var _ = require('../../my_lodash/my_lodash.js');
 
 function even_to_letter(collection) {
-    var result = _.filter_in(collection,function (num) {
-        return num % 2 === 0;
-    });
-    _.each(result,function (num,i) {
-        result[i] = _.num_to_letter(num);
-    });
-
-    return result;
+    return _(collection).filter(function (item,i) {
+        return item % 2 === 0;
+    }).map(function (item) {
+        return _().num_to_letter(item).value();
+    }).value();
 }
 
 module.exports = even_to_letter;
