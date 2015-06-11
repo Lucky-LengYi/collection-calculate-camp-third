b38a86cd1a7ff9a3ede93600b7750230fbdcda2a
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-06-11 09:49:20 +0800

    完成本函数

--- practices/collectionOperator/get_intersection.js
+++ practices/collectionOperator/get_intersection.js
@@ -3,9 +3,9 @@ var _ = require('../my_lodash/my_lodash.js');
 
 function get_intersection(collection_a, collection_b) {
     var result = [];
-    _.each(collection_b,function (item,i) {
-        if (_.exist(collection_a,item)) {
-            result[result.length] = item;
+    _(collection_b).each(function (item,i) {
+        if (_(collection_a).exist(item).value()) {
+            result.push(item);
         }
     });
     return result;
