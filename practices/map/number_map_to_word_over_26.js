794be2992e4ee366bb90e997588294c4da8fb5ad
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-06-11 10:39:31 +0800

    complete map/number_map_to_word_over_26

--- practices/map/number_map_to_word_over_26.js
+++ practices/map/number_map_to_word_over_26.js
@@ -2,10 +2,9 @@
 var _ = require('../my_lodash/my_lodash.js');
 
 var number_map_to_word_over_26 = function(collection){
-    var result = _.map(collection,function (number) {
-        return _.num_to_letter(number);
-    });
-    return result;
+    return _(collection).map(function (item) {
+        return _().num_to_letter(item).value();
+    }).value();
 };
 
 module.exports = number_map_to_word_over_26;
