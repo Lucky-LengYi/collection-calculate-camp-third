94702978615bc91910c7bb706fe107647cbb2ce9
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-06-11 12:11:55 +0800

    complete superposition_operation/collection_operation/practice_1

--- practices/superposition_operation/collection_operation/practice_1.js
+++ practices/superposition_operation/collection_operation/practice_1.js
@@ -1,11 +1,9 @@
 'use strict';
 var _ = require('../../my_lodash/my_lodash.js');
 function hybrid_operation(collection) {
-    var three_times_add_two = _.map(collection,function (num) {
-        return num * 3 + 2;
-    });
-    var sum = _.sum(three_times_add_two);
-    return sum;
+    return _(collection).map(function (item) {
+        return item * 3 + 2;
+    }).sum().value();
 }
 
 module.exports = hybrid_operation;
