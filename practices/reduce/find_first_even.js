afeed187b893e47a24ea4920454114d8e7d1c6a1
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-06-11 11:18:51 +0800

    complete reduce/find_first_even

--- practices/reduce/find_first_even.js
+++ practices/reduce/find_first_even.js
@@ -2,9 +2,9 @@
 var _ = require('../my_lodash/my_lodash.js');
 
 function find_first_even(collection) {
-    return _.first(collection,function (num) {
+    return _(collection).first(function (num) {
         return num % 2 === 0;
-    });
+    }).value();
 }
 
 module.exports = find_first_even;
