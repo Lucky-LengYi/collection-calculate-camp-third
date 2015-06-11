d3ef48f6c213d4189e6b839946d752da6e1fd12e
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-06-11 12:57:23 +0800

    complete own~/sub~/caculate_median

--- practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
@@ -1,14 +1,14 @@
 'use strict';
 var _ = require('../../../my_lodash/my_lodash.js');
 var calculate_median = function(collection){
-    var median;
     var result = [];
-    _.each(collection,function (num,i) {
+    _(collection).each(function (item,i) {
         if (i % 2 === 1) {
-            result[result.length] = num;
+            result.push(item);
         }
     });
-    result = _.bubble_sort(result);
-    return _.get_median(result);
+    return _(result).bubble_sort(function (item_a,item_b) {
+        return item_a > item_b;
+    }).get_median().value();
 };
 module.exports = calculate_median;
