202ed5ded01e83fe5d3a8e603c02577630f8e701
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-06-11 13:13:27 +0800

    complete the single_element && complete the collection-caculate-camp third times

--- practices/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
@@ -2,11 +2,11 @@
 var _ = require('../../../my_lodash/my_lodash.js');
 var single_element = function(collection){
     var result = [];
-    result = _.filter_in(collection,function (num,i) {
-        return ((i + 1) % 2) === 0;
-    });
+    result = _(collection).filter(function (item,i) {
+        return i % 2 === 1;
+    }).value();
     var temp = {};
-    _.each(result,function (num,i) {
+    _(result).each(function (num,i) {
         temp[num+'_'] = temp[num+'_'] || 0;
         temp[num+'_'] = temp[num+'_'] + 1;
     });
