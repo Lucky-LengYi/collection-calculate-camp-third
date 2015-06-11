572b0f98e99fcbf73bdefd8d8174fbb5020b4575
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-06-11 10:35:10 +0800

    complete map/map_to_four_multiples_add_one.js

--- practices/map/map_to_four_multiples_add_one.js
+++ practices/map/map_to_four_multiples_add_one.js
@@ -2,10 +2,9 @@
 var _ = require('../my_lodash/my_lodash.js');
 
 var map_to_four_multiples_add_one = function(collection){
-    var result = _.map(collection,function (num) {
-        return num * 4 + 1;
-    });
-    return result;
+    return _(collection).map(function (item) {
+        return item * 4 + 1;
+    }).value();
 };
 
 module.exports = map_to_four_multiples_add_one;
