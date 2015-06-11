73f76ef30cebd0b970124c8aee2796cf46c75d17
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-06-11 10:43:30 +0800

    complete map/rank_asc

--- practices/map/rank_asc.js
+++ practices/map/rank_asc.js
@@ -2,13 +2,9 @@
 var _ = require('../my_lodash/my_lodash.js');
 
 var rank_asc = function(collection){
-    var result_sort = _.bubble_sort(collection);
-    var len = result_sort.length;
-    var result = [];
-    for (;len--;) {
-        result[result.length] = result_sort[len];
-    }
-    return result;
+    return _(collection).bubble_sort(function (item_a,item_b) {
+        return item_a < item_b;
+    }).value();
 };
 
 module.exports = rank_asc;
