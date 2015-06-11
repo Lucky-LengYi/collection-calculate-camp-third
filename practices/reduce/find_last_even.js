ad01a92576995f9fbad83f691d71614cbcb37916
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-06-11 11:20:26 +0800

    complete reduce/find_last_even

--- practices/reduce/find_last_even.js
+++ practices/reduce/find_last_even.js
@@ -2,9 +2,9 @@
 var _ = require('../my_lodash/my_lodash.js');
 
 function find_last_even(collection) {
-    return _.last(collection,function (num) {
+    return _(collection).last(function (num) {
         return num % 2 === 0;
-    });
+    }).value();
 }
 
 module.exports = find_last_even;
