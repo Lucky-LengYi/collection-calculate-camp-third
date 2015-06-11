ef8803be1339d2f6bf57f3ebd5ae5f3ef86694e7
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-06-11 12:09:50 +0800

    complete superposition_operation/collection_operation/letter_mapping_2 && add ';'

--- practices/superposition_operation/collection_operation/letter_mapping_2.js
+++ practices/superposition_operation/collection_operation/letter_mapping_2.js
@@ -2,7 +2,7 @@
 var _ = require('../../my_lodash/my_lodash.js');
 
 function average_to_letter(collection) {
-    var sum = _(collection).sum().value()
+    var sum = _(collection).sum().value();
     var average = Math.ceil(sum/collection.length);
     return  _().num_to_letter(average).value();
 }
