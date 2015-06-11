ab2a033d26880f3ec371fb6ae2756c485695fc2d
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-06-11 12:22:02 +0800

    complete superposition_operation/interval_operation/average_uneven

--- practices/superposition_operation/interval_operation/average_uneven.js
+++ practices/superposition_operation/interval_operation/average_uneven.js
@@ -1,11 +1,10 @@
 'use strict';
 var _ = require('../../my_lodash/my_lodash.js');
 function average_uneven(collection) {
-    var result_array = _.filter_in(collection,function (num) {
-        return num % 2 === 1;
-    });
-    var sum = _.sum(result_array);
-    return sum / result_array.length;
+    var result_array = _(collection).filter(function (item,i) {
+        return item % 2 === 1;
+    }).value();
+    return _(result_array).sum().value() / result_array.length;
 }
 
 module.exports = average_uneven;
