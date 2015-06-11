58cca5bc3679a6f7436033eefd2f0da864443b9c
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-06-11 11:11:50 +0800

    complete reduce/compute_sum

--- practices/reduce/compute_sum.js
+++ practices/reduce/compute_sum.js
@@ -2,7 +2,7 @@
 var _ = require('../my_lodash/my_lodash.js');
 
 function calculate_elements_sum(collection) {
-    return _.sum(collection);
+    return _(collection).sum().value();
 }
 
 module.exports = calculate_elements_sum;
